6def71289d5a1ce9580b2610e8f623178d70b42e
Taninchot Phuwaloertthiwat <taninchot.art@mail.kmutt.ac.th>
2021-11-20 23:59:09 +0700

    [UPDATE] Controller for board

---
+++ src/controllers/boardController.js
@@ -0,0 +1,190 @@
+const { board } = require("../models/model")
+const { validateBoard } = require("../helpers/validation")
+const { readFile, deleteFile } = require("../helpers/file")
+const { calPage, calSkip } = require('../helpers/pagination');
+
+let findAllBoard = async () => {
+  let result
+  try {
+    result = await board.findMany({
+      select: {
+        b_id: true,
+        b_title: true,
+        b_caption: true,
+        b_image: true,
+        account: {
+          select: {
+            ac_id: true,
+            ac_username: true,
+            ac_fname: true,
+            ac_lname: true,
+            ac_image: true
+          }
+        }
+      }
+    })
+  } catch (error) {
+    throw new Error(error.message)
+  }
+  return result
+}
+
+let getBoardByPage = async (page, numberOfItem) => {
+  let results
+  let totalPage
+  try {
+    results = await board.findMany({
+      select: {
+        b_id: true,
+        b_title: true,
+        b_caption: true,
+        b_image: true,
+        account: {
+          select: {
+            ac_id: true,
+            ac_username: true,
+            ac_fname: true,
+            ac_lname: true,
+            ac_image: true
+          }
+        }
+      },
+      orderBy: {
+        b_id: "desc"
+      },
+      skip: calSkip(page, numberOfItem),
+      take: numberOfItem,
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+  const totalBoard = await board.count()
+  totalPage = calPage(totalBoard, numberOfItem)
+  return { results, totalPage }
+}
+
+let addBoard = async (files, accountId) => {
+  let imgFile = []
+  let jsonFile = files.find((file) => {
+    if (!file.mimetype != "application/json") {
+      imgFile.push(file)
+    }
+    return file.mimetype == "application/json"
+  })
+  if (!jsonFile) {
+    throw new Error("Please send jsonData")
+  }
+  let boardData
+  try {
+    boardData = await readBoardData(jsonFile, imgFile, null, accountId)
+  } catch (error) {
+    throw new Error(error)
+  }
+
+  try {
+    await board.create({
+      data: boardData
+    })
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+let editBoard = async (files, id, accountId) => {
+  let findedBoard
+  try {
+    findedBoard = await board.findFirst({
+      where: {
+        b_id: id
+      },
+      select: {
+        b_image: true,
+        account_id: true
+      }
+    })
+  } catch (error) {
+    throw new Error(error.message)
+  }
+
+  if (!findedBoard) {
+    throw new Error("Can't find board ID")
+  }
+
+  if (findedBoard.account_id != accountId) {
+    throw new Error("Can't edit another board not own")
+  }
+
+  let imgFile = []
+  let jsonFile = files.find((file) => {
+    if (!file.mimetype != "application/json") {
+      imgFile.push(file)
+    }
+    return file.mimetype == "application/json"
+  })
+  let boardData
+  try {
+    boardData = await readBoardData(jsonFile, imgFile, null, accountId)
+  } catch (error) {
+    throw new Error(error)
+  }
+
+  try {
+    await board.update({
+      where: {
+        b_id: id
+      },
+      data: boardData
+    })
+  } catch (error) {
+    throw new Error(error)
+  }
+
+  if (findedBoard.b_image != "") {
+    await deleteFile(findedBoard.b_image)
+  }
+}
+
+let readBoardData = async (jsonFile, imgFile, findedBoard, accountId) => {
+  let boardData = await readFile(jsonFile)
+  deleteFile(jsonFile.filename)
+  if (findedBoard) {
+    if (!findedBoard["b_image"]) {
+      findedBoard["b_image"] = ""
+    }
+    boardData["b_image"] = findedBoard["b_image"]
+  }
+  for (const [index, img] of imgFile.entries()) {
+    if (img.fieldname == "b_image") {
+      boardData["b_image"] = await img.filename
+    }
+  }
+  boardData["account_id"] = accountId
+  const { error } = validateBoard(boardData)
+  if (error) {
+    throw new Error(error.details[0].message)
+  }
+  return boardData
+}
+
+let deleteBoard = async (id, accountId) => {
+  let result
+  try {
+    result = await board.deleteMany({
+      where: {
+        AND: {
+          b_id: id,
+          account_id: accountId
+        }
+      }
+    })
+    if (result.count <= 0) {
+      throw new Error("Can't delete another board not own")
+    }
+  } catch (error) {
+    throw new Error(error)
+  }
+  if (result["b_image"]) {
+    deleteFile(result["b_image"])
+  }
+}
+module.exports = { findAllBoard, getBoardByPage, addBoard, editBoard, deleteBoard }
--- src/routes/board.js
+++ src/routes/board.js
@@ -1,183 +1,68 @@
 const router = require("express").Router()
-const { board } = require("../models/model")
-const { validateBoard } = require("../helpers/validation")
 const upload = require("../middlewares/uploadFile")
-const { readFile, deleteFile, dataNotValid } = require("../helpers/file")
-const { calPage, calSkip } = require('../helpers/pagination');
+const { dataNotValid } = require("../helpers/file")
+const boardController = require("../controllers/boardController")
+const auth = require("../middlewares/auth")
+
 
 router.get("/", async (req, res) => {
   let results
   try {
-    results = await board.findMany({
-      include: {
-        account: true
-      }
-    })
+    results = await boardController.findAllBoard()
   } catch (error) {
-    return res.send({ status: "Can't get data", error: error })
+    return res.send({ error: error.message })
   }
-  return res.send({ data: results, totalPage: calPage(results.length) })
+  return res.send({ data: results })
 })
 
-router.get("/page/:page", async (req, res) => {
+router.get("/page/:item/:page", async (req, res) => {
   let page = Number(req.params.page)
-  let numberOfItem = 6
+  let numberOfItem = Number(req.params.item)
   let results
+  let totalPage
   try {
-    results = await board.findMany({
-      orderBy: {
-        b_id: "desc"
-      },
-      skip: calSkip(page, numberOfItem),
-      take: numberOfItem,
-    })
+    let data = await boardController.getBoardByPage(page, numberOfItem)
+    results = data.results
+    totalPage = data.totalPage
   } catch (error) {
-    return res.status(500).send({ error: error })
+    return res.status(500).send({ error: error.message })
   }
-  const totalBoard = await board.count()
-  if (!results) {
-    return res.send({ data: results })
-  }
-  return res.send({ data: results, page: page, totalPage: calPage(totalBoard, numberOfItem) })
+  return res.send({ data: results, page: page, totalPage: totalPage })
 })
 
-router.post('/add', upload, async (req, res) => {
+router.post('/add', auth, upload, async (req, res) => {
   let files = req.files
-  let imgFile = []
-  let jsonFile = files.find((file) => {
-    if (!file.mimetype != "application/json") {
-      imgFile.push(file)
-    }
-    return file.mimetype == "application/json"
-  })
-  if (!jsonFile) {
+  let accountId = req.account["ac_id"]
+  try {
+    await boardController.addBoard(files, accountId)
+  } catch (error) {
     await dataNotValid(files)
-    return res.status(500).send({ msg: `Please send jsonData` })
+    return res.status(500).send({ error: error.message })
   }
-  let boardData
-  try {
-    boardData = await readFile(jsonFile)
-    deleteFile(jsonFile.filename)
-    for (const [index, img] of imgFile.entries()) {
-      if (img.fieldname == "b_image") {
-        boardData.b_image = await img.filename
-      }
-    }
-    const { error } = validateBoard(boardData)
-    if (error) {
-      return res.status(500).send({ err: error.details[0].message })
-    }
-  } catch (error) {
-    return res.status(500).send({ error: error })
-  }
-
-  try {
-    await board.create({
-      data: boardData
-    })
-  } catch (error) {
-    if (error.code == 'P2003') {
-      console.log(error.message)
-      return res.status(500).send({ msg: "Foreign key constraint failed on the field: `account_ac_id`" })
-    }
-    return res.status(500).send({ error: error })
-  }
-  return res.send({ status: "Create Board Successfully", err: false })
+  return res.send({ status: "Create Board Successfully" })
 })
 
-router.put("/edit/:id", upload, async (req, res) => {
+router.put("/edit/:id", auth, upload, async (req, res) => {
   let id = Number(req.params.id)
-
-  let findedBoard
-  try {
-    findedBoard = await board.findFirst({
-      where: {
-        b_id: id
-      },
-      select: {
-        b_image: true,
-      }
-    })
-  } catch (error) {
-    return res.status(500).send({ error: error })
-  }
-
-  if (!findedBoard) {
-    return res.status(500).send({ msg: "Can't find board id" })
-  }
-
   let files = req.files
-  let imgFile = []
-  let jsonFile = files.find((file) => {
-    if (!file.mimetype != "application/json") {
-      imgFile.push(file)
-    }
-    return file.mimetype == "application/json"
-  })
-  if (!jsonFile) {
+  let accountId = req.account["ac_id"]
+  try {
+    await boardController.editBoard(files, id, accountId)
+  } catch (error) {
     await dataNotValid(files)
-    return res.status(500).send({ msg: `Please send jsonData` })
-  }
-  let boardData
-  try {
-    boardData = await readFile(file)
-    deleteFile(file.filename)
-    if (!findedBoard.b_image) {
-      findedBoard.b_image = ""
-    }
-    boardData.b_image = findedBoard.b_image
-    for (const [index, img] of imgFile.entries()) {
-      if (img.fieldname == "b_image") {
-        boardData.b_image = await img.filename
-      }
-    }
-    const { error } = validateBoard(boardData)
-    if (error) {
-      return res.status(500).send({ err: error.details[0].message })
-    }
-  } catch (error) {
-    return res.status(500).send({ error: error })
+    return res.status(500).send({ error: error.message })
   }
 
-
-  let updateResult
-  try {
-    updateResult = await board.update({
-      where: {
-        b_id: id
-      },
-      data: boardData
-    })
-  } catch (error) {
-    if (error.code == 'P2003') {
-      return res.status(500).send({ msg: "Foreign key constraint failed on the field: `account_ac_id`" })
-    }
-  }
-
-  if (updateResult) {
-    for (const [index, img] of imgFile.entries()) {
-      if (findedBoard.b_image != "") {
-        await deleteFile(findedBoard.b_image)
-      }
-    }
-  }
   return res.send({ status: "Update Board Successfully", err: false })
 })
 
-router.delete("/delete/:id", async (req, res) => {
+router.delete("/delete/:id", auth, async (req, res) => {
   let id = Number(req.params.id)
-  let result
+  let accountId = req.account["ac_id"]
   try {
-    result = await board.delete({
-      where: {
-        b_id: id
-      }
-    })
-  } catch (err) {
-    return res.status(500).send({ msg: err })
-  }
-  if (result.b_image) {
-    deleteFile(result.b_image)
+    await boardController.deleteBoard(id, accountId)
+  } catch (error) {
+    return res.status(500).send({ error: error.message })
   }
   return res.send({ msg: "Delete board successfully" })
 })
